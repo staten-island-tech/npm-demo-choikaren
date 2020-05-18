648a9ccc135b7a0303a1c682630ad0fcf042aacc
choikaren <57500302+choikaren@users.noreply.github.com>
2020-05-17 22:14:26 -0400

    fixed mission statement

--- dist/index.js
+++ dist/index.js
@@ -212,7 +212,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "53890" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "56220" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
